ffb4f965e91c087c1c25fc3816221658c0bc21ee
ImRaphZ <60404757+ImRaphZ@users.noreply.github.com>
2026-05-26 16:42:13 +0100

    More game was made, probably

--- game.gd
+++ game.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 @onready var introduction = $Introduction
-@onready var animation_player = $AnimationPlayer 
 
 var current_panel = "Introduction"
 
 func _on_ready() -> void:
 	introduction.show()
-	animation_player.play("Introduction")
+	introduction.get_node("AnimationPlayer").play("Introduction")
 
 
 func changePanel(new_panel: String) -> void:
@@ -18,8 +17,8 @@ func changePanel(new_panel: String) -> void:
 	var new_node = get_node(new_panel)
 	new_node.show()
 	
-	if (animation_player.has_animation(new_panel)):
-		animation_player.play(new_panel)
+	if (new_node.has_node("AnimationPlayer")):
+		new_node.get_node("AnimationPlayer").play(new_panel)
 
 	current_panel = new_panel
 	#pass
