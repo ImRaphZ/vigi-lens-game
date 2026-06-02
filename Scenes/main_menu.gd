56241d0df3453ceeaba5730523aad600d970deb7
Rafael Dias <60404757+ImRaphZ@users.noreply.github.com>
2026-06-02 23:01:42 +0100

    Add Main Menu

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _on_play_button_pressed() -> void:
+	$Title.visible = false
+	$PlayButton.visible = false
+	$ExitButton.visible = false
+	$Loading.visible = true
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
